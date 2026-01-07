a9e060568ec7f5a99fcb4ebe0f68ae572418f778
Seniv Volodymyr <seniv.05@gmail.com>
2026-01-07 15:19:24 +0100

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginClicked(object? sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                LoginBtn.Text = $"Clicked {count} time login";
+            else
+                LoginBtn.Text = $"Clicked {count} times login";
+
+            SemanticScreenReader.Announce(LoginBtn.Text);
+        }
+        private void RegisterClicked(object? sender, EventArgs e) 
+        {
+            count++;
+
+            if (count == 1)
+                RegisterBtn.Text = $"Clicked {count} time register";
+            else
+                RegisterBtn.Text = $"Clicked {count} times register";
+
+            SemanticScreenReader.Announce(RegisterBtn.Text);
+        }
+    }
+}
